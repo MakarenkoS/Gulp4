299f98cf1fe31b65db1ea75bcecb67e8f1433632
Sergey <Rsmon1982@yandex.ru>
2021-06-19 14:30:22 +0400

    Add pluggins for HTML and SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ let path = {
     },
 
     src: {
-        html: sourceFolder + "/",
+        html: [sourceFolder + "/*.html", "!" + sourceFolder + "/_*.html"],
         css: sourceFolder + "/scss/style.scss",
         js: sourceFolder + "/js/script.js",
         img: sourceFolder + "/img/**/*.{jpg, png, svg, git, ico, webp}",
@@ -29,10 +29,20 @@ let path = {
 
 }
 
+// ****************************************Variables
+
 let {src, dest} = require("gulp"),
     gulp = require("gulp"),
-    browser_sync = require("browser-sync").create();
+    browser_sync = require("browser-sync").create(),
+    file_include = require("gulp-file-include"),
+    del = require('del'),
+    scss = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    group_media = require('gulp-group-css-media-queries'),
+    clean_css = require('gulp-clean-css'),
+    gulp_rename = require('gulp-rename')
 
+// ****************************************
 
 function browserSync(params) {
     browser_sync.init({
@@ -44,8 +54,64 @@ function browserSync(params) {
     })
 }
 
+// ****************************************
 
-let watch = gulp.parallel(browserSync)
+function html() {
+    return src(path.src.html)
+        .pipe(file_include())
+        .pipe(dest(path.build.html))
+        .pipe(browser_sync.stream())
+}
 
+// ****************************************
+
+function css() {
+    return src(path.src.css)
+        .pipe(scss({
+            outputStyle: "expanded"
+        }))
+        .pipe(
+            group_media()
+        )
+        .pipe(
+            autoprefixer({
+                cascade: true
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(
+            clean_css()
+        )
+        .pipe(
+            gulp_rename({
+                extname: ".min.css"
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browser_sync.stream())
+}
+
+// ****************************************
+
+function watcher(params) {
+    gulp.watch([path.watch.html], html)
+    gulp.watch([path.watch.css], css)
+}
+
+// ****************************************
+
+function clean(params) {
+    return del(path.clean)
+}   
+
+
+
+let build = gulp.series(clean, gulp.parallel(html, css));
+let watch = gulp.parallel(build, watcher, browserSync);
+
+
+exports.css = css;
+exports.html = html;
+exports.build = build;
 exports.watch = watch;
-exports.default = watch
+exports.default = watch;
